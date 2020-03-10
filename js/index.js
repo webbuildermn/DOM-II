559d4805cd69820593c42e1161de3d1ee5b47612
Webbuildermn <gerald.ryan40@yahoo.com>
2020-03-10 18:00:31 -0500

    Made it colorful

--- js/index.js
+++ js/index.js
@@ -5,29 +5,48 @@ let totalClicks = 0
 var theBody = document.querySelector('body')
 
 theBody.addEventListener('click',
-    ()=>{
-      totalClicks++
-      if (totalClicks ==15){
-        alert("5 more clicks left")
-      }
-      if (totalClicks ==20){
-        theBody.innerHTML = "Access Denied"
-        
-        theBody.style.backgroundColor="red"
+    () => {
+        totalClicks++
+        if (totalClicks == 15) {
+            alert("5 more clicks left")
+        }
+        if (totalClicks == 20) {
+            theBody.innerHTML = "You've arrived"
 
-      }
+            theBody.style.backgroundColor = "green"
+            theBody.style.fontSize = "8rem";
+            theBody.style.color = "white"
+
+        }
+    }
+)
+
+header.addEventListener('dblclick',
+    () => {
+        header.style.backgroundColor = "yellow"
+        alert("you spilled the mustard")
+        header.style.tranform = "scaleX(-1)"
+        header.style.transition = "transform 1s"
     }
 
 )
 
+theBody.addEventListener('wheel',
+    (eveny) => {
+      console.log(event.deltaY)
+      let r = Math.floor(Math.random()*255)
+      let g = Math.floor(Math.random()*255)
+      let b = Math.floor(Math.random()*255)
 
+        containerHome.style.backgroundColor = `rgb(${r},${g},${b}`;
 
+    })
 
 bus.addEventListener(
     'mouseenter',
     () => {
-        bus.style.transition = "transform 1"
         bus.style.transform = "scaleX(-1)";
+        bus.style.transition = "transform 1s"
         bus.style.backfaceVisibility = "hidden"; //does not work
 
     }
@@ -55,17 +74,16 @@ bus.addEventListener(
     //     bus.style.transform ="rotateY(180deg)"
     // }
     // )
-let clickCount = 1
 
 let images = document.querySelectorAll("section img")
-console.log(images.length)
 
 for (var i = 0; i < images.length; i++) {
 
     images[i].addEventListener(
         'click',
         function() {
-            if (clickCount > 0) {
+            // console.log(this.style)
+            if (this.style.opacity === "1") {
                 this.style.opacity = "0%"
                 this.style.transition = "opacity .5s"
                 console.log(images[i])
@@ -73,7 +91,24 @@ for (var i = 0; i < images.length; i++) {
                 this.style.opacity = "100%"
                 this.style.transition = "opacity .5s"
             }
-            clickCount *= -1
         }
     )
 }
+
+// for (var i = 0; i < images.length; i++) {
+
+//   images[i].addEventListener(
+//       'click',
+//       function() {
+//           if (clickCount > 0) {
+//               this.style.opacity = "0%"
+//               this.style.transition = "opacity .5s"
+//               console.log(images[i])
+//           } else {
+//               this.style.opacity = "100%"
+//               this.style.transition = "opacity .5s"
+//           }
+//           clickCount *= -1
+//       }
+//   )
+// }
