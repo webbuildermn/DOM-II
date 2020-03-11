ca42965f7e9ec3f555b56d6564a41a721d614920
Webbuildermn <gerald.ryan40@yahoo.com>
2020-03-11 10:33:40 -0500

    Got new videos connected.

--- js/index.js
+++ js/index.js
@@ -21,8 +21,8 @@ theBody.addEventListener('click',
     }
 )
 
-window.addEventListener('scroll',(event)=>{
-  console.log("you scrolled")
+window.addEventListener('scroll', (event) => {
+    console.log("you scrolled")
 })
 
 header.addEventListener('dblclick',
@@ -37,10 +37,10 @@ header.addEventListener('dblclick',
 
 theBody.addEventListener('wheel',
     (eveny) => {
-      console.log(event.deltaY)
-      let r = Math.floor(Math.random()*255)
-      let g = Math.floor(Math.random()*255)
-      let b = Math.floor(Math.random()*255)
+        console.log(event.deltaY)
+        let r = Math.floor(Math.random() * 255)
+        let g = Math.floor(Math.random() * 255)
+        let b = Math.floor(Math.random() * 255)
 
         containerHome.style.backgroundColor = `rgb(${r},${g},${b}`;
 
@@ -65,13 +65,13 @@ bus.addEventListener(
 
 let n = -1;
 bus.addEventListener(
-        'click',
-        () => {
-            bus.style.transform = `scaleY(${n})`
-            bus.style.transition = "transform 1.25s"
-            n *= -1
-        }
-    )
+    'click',
+    () => {
+        bus.style.transform = `scaleY(${n})`
+        bus.style.transition = "transform 1.25s"
+        n *= -1
+    }
+)
 
 let images = document.querySelectorAll("section img")
 
@@ -93,13 +93,13 @@ for (var i = 0; i < images.length; i++) {
     )
 }
 
-window.addEventListener('keypress',(event)=> {alert(event.charCode + " was pressed")})
+window.addEventListener('keypress', (event) => { alert(event.charCode + " was pressed") })
 
 let h2all = document.querySelectorAll("h2")
 
 for (var i = 0; i < h2all.length; i++) {
 
-  h2all[i].addEventListener(
+    h2all[i].addEventListener(
         'select',
         function() {
             console.log(this.style)
@@ -116,29 +116,48 @@ for (var i = 0; i < h2all.length; i++) {
     )
 }
 
-window.addEventListener('load',(event)=>{
-  alert("you get 20 clicks. Use them wisely")
+window.addEventListener('load', (event) => {
+    alert("you get 20 clicks. Use them wisely")
 
 })
 
 let footer = document.querySelector('footer')
-footer.addEventListener("dblclick",()=>{
-  footer.innerHTML="Access Denied"
-  footer.style.backgroundColor = "red"
-  footer.style.color = "white"
-  footer.style.fontSize = "3rem"
-  footer.style.textAlign = "center"
-  footer.style.padding = "15px 0 15px 0"
+footer.addEventListener("dblclick", () => {
+    footer.innerHTML = "Access Denied"
+    footer.style.backgroundColor = "red"
+    footer.style.color = "white"
+    footer.style.fontSize = "3rem"
+    footer.style.textAlign = "center"
+    footer.style.padding = "15px 0 15px 0"
 
 })
 
-secondbtn.addEventListener('click', function (event) {
-let alice = document.createElement("iframe")
-alice.setAttribute('src','https://www.youtube.com/embed/AqIehvpdAPI')
-console.log(alice)
-theBody.appendChild(alice)
+secondbtn.addEventListener('click', function(event) {
+    let alice = document.createElement("iframe")
+    alice.setAttribute('src', 'https://www.youtube.com/embed/AqIehvpdAPI')
+    console.log(alice)
+    this.parentNode.replaceChild(alice, this)
 
 })
+
+function addVideo(buttonID, URL) {
+    buttonID.addEventListener('click', function(event) {
+        let alice = document.createElement("iframe")
+        alice.setAttribute('src', URL)
+        this.parentNode.replaceChild(alice, this)
+
+    })
+}
+
+addVideo(thirdbtn,"https://www.youtube.com/embed/hc7joL3muqM")
+addVideo(firstbtn,"https://www.youtube.com/embed/b5HgF_1Sihc")
+
 
 // Can I put all this in an iframe object straight away at once?
-{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/AqIehvpdAPI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+{ /* <iframe width="560" height="315" src="https://www.youtube.com/embed/AqIehvpdAPI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */ }
+
+{ /* <iframe width="560" height="315" src="https://www.youtube.com/embed/hc7joL3muqM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */ }
+
+{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/jPyYGw9Jn6w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+
+{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/b5HgF_1Sihc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
